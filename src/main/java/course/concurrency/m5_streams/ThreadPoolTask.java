c9b7f4e6090df65f949f3dbebc02cf520ec135d1
rgrigorev <roma.grigoriev.2015@inbox.ru>
2023-09-27 23:11:44 +0400

    Resolved queues and thread pool tasks

--- src/main/java/course/concurrency/m5_streams/ThreadPoolTask.java
+++ src/main/java/course/concurrency/m5_streams/ThreadPoolTask.java
@@ -6,11 +6,38 @@ public class ThreadPoolTask {
 
     // Task #1
     public ThreadPoolExecutor getLifoExecutor() {
-        return null;
+        return new ThreadPoolExecutor(
+                1,
+                1,
+                10,
+                TimeUnit.SECONDS,
+                new LinkedBlockingDeque<>() {
+                    @Override
+                    public boolean offer(Runnable runnable) {
+                        return super.offerFirst(runnable);
+                    }
+                }
+        );
     }
 
     // Task #2
     public ThreadPoolExecutor getRejectExecutor() {
-        return null;
+        return new ThreadPoolExecutor(
+                8,
+                8,
+                10,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(1) {
+
+                    @Override
+                    public boolean offer(Runnable runnable) {
+                        return false;
+                    }
+                },
+                (r, c) -> {
+                    System.out.println("Discarded");
+                }
+        );
     }
+
 }
